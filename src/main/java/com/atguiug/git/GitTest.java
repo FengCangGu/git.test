3d452eeb8c10da656d0a64ce83ed42d72ad34761
gufengcang <gufengcang@atguigu.com>
2021-08-23 23:22:39 +0800

    first commit

---
+++ src/main/java/com/atguiug/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguiug.git;
+
+/**
+ * @author GuFengCang
+ * @create 2021-23-23:20
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git11111");
+    }
+}
