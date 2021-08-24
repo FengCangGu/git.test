50f2fbd4dc4d372bc21ef9b0162cfeadc09f27f9
FengCangGu <89339436+FengCangGu@users.noreply.github.com>
2021-08-24 10:18:49 +0800

    pull commit

--- src/main/java/com/atguiug/git/GitTest.java
+++ src/main/java/com/atguiug/git/GitTest.java
@@ -13,5 +13,7 @@ public class GitTest {
         System.out.println("master.git5555");
         System.out.println("hot-fix.test444");
         System.out.println("push...test444");
+        System.out.println("pull...test555");
+        
     }
 }
